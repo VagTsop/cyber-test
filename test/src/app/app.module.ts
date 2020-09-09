6111d93bb56defaac8b827be23e1f4324f8735c9
vagg tsop <vatsop52@gmail.com>
2020-09-09 21:26:13 +0300

    create basic-auth.interceptor

--- test/src/app/app.module.ts
+++ test/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { ListComponent } from './list/list.component';
+import { BasicAuthInterceptor } from './helpers/basic-auth.interceptor.service';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 
 
@@ -24,7 +26,12 @@ import { ListComponent } from './list/list.component';
     AppRoutingModule,
     ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS,
+      useClass: BasicAuthInterceptor,
+      multi: true 
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
