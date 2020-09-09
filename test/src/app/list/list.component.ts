3aa4bf3bc17f736acf2d09911523063a16699ce8
vagg tsop <vatsop52@gmail.com>
2020-09-09 23:42:16 +0300

    create welcome message and get all users in home component template

--- test/src/app/list/list.component.ts
+++ test/src/app/list/list.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import instructions from '../../assets/config/instructions.js';
+import { User } from '../models/user.js';
+import { UserService } from '../services/user.service.js';
+import { first } from 'rxjs/operators';
 
 @Component({
   selector: 'app-list',
@@ -7,11 +10,21 @@ import instructions from '../../assets/config/instructions.js';
   styleUrls: ['./list.component.scss']
 })
 export class ListComponent implements OnInit {
-
+  loading = false;
+  users: User[];
   instructions = instructions.list;
-  constructor() { }
+  
+  constructor(private userService: UserService) { }
+
+
+  
+ 
 
   ngOnInit() {
+    this.loading = true;
+    this.userService.getAll().pipe(first()).subscribe(users => {
+        this.loading = false;
+        this.users = users;
+      });
   }
-
 }
