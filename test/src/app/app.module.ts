9aaebf29e9169f201c921608167d5c50ec5f9a7f
vagg tsop <vatsop52@gmail.com>
2020-09-12 20:44:27 +0300

    create header component

--- test/src/app/app.module.ts
+++ test/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { ListComponent } from './list/list.component';
+import { HeaderComponent } from './header/header.component';
 
 
 
@@ -16,7 +17,8 @@ import { ListComponent } from './list/list.component';
     AppComponent,
     LoginComponent,
     HomeComponent,
-    ListComponent
+    ListComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
