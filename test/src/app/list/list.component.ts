35697682c9dcb1c61b197c9f085bb578db722419
vagg tsop <vatsop52@gmail.com>
2020-09-13 22:58:57 +0300

    read book json file

--- test/src/app/list/list.component.ts
+++ test/src/app/list/list.component.ts
@@ -1,17 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import instructions from '../../assets/config/instructions.js';
+import { BooksService } from './books.service.js';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss']
+  styleUrls: ['./list.component.scss'],
+  providers: [BooksService],
 })
 export class ListComponent implements OnInit {
-
+  booksList: string [] ;
   instructions = instructions.list;
-  constructor() { }
 
-  ngOnInit() {
-  }
+  constructor( private route: ActivatedRoute, 
+    private booksService: BooksService) { }
 
+    ngOnInit(){
+      this.booksService.getJSON().subscribe(data => {
+           console.log(data);
+       });
+    }
 }
